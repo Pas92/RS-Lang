4053bb1d7162525f8955a50b9ea364b86dfe6e49
Pas92 <pavelburdovitsin92@gmail.com>
2022-08-17 11:04:16 +0300

    feat: create project structure

--- rs-lang/src/app/app.module.ts
+++ rs-lang/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AudioChallengeComponent } from './pages/audio-challenge/audio-challenge.component';
+import { WordCardComponent } from './components/word-card/word-card.component';
+import { SprintComponent } from './pages/sprint/sprint.component';
+import { TextbookComponent } from './pages/textbook/textbook.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { GameResultsComponent } from './shared/components/game-results/game-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AudioChallengeComponent,
+    WordCardComponent,
+    SprintComponent,
+    TextbookComponent,
+    StatisticsComponent,
+    GameResultsComponent
   ],
   imports: [
     BrowserModule,
