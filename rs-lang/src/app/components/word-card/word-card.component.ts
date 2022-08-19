60eec1fefa52f8b3158b37ebc1a6bc603cf6e556
Pas92 <pavelburdovitsin92@gmail.com>
2022-08-19 16:43:28 +0300

    feat: create wordCard component

--- rs-lang/src/app/components/word-card/word-card.component.ts
+++ rs-lang/src/app/components/word-card/word-card.component.ts
@@ -1,14 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { WordData } from 'src/app/models/requests.model';
+import { BASE_URL } from 'src/app/models/requests.model';
 
 @Component({
   selector: 'app-word-card',
   templateUrl: './word-card.component.html',
   styleUrls: ['./word-card.component.scss']
 })
-export class WordCardComponent implements OnInit {
+export class WordCardComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+
+  baseURL = BASE_URL
+  audioWord: HTMLAudioElement = new Audio()
+  audioExample: HTMLAudioElement = new Audio()
+  audioMeaning: HTMLAudioElement = new Audio()
+
+  playStates = {
+    audioWord:  false,
+    audioMeaning:  false,
+    audioExample:  false
+  }
 
   @Input() wordData: WordData = {
     _id: 'unknown',
@@ -28,6 +40,36 @@ export class WordCardComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  playAudio(): void {
+    this.audioWord.src = `${this.baseURL}/${this.wordData.audio}`
+    this.audioMeaning.src = `${this.baseURL}/${this.wordData.audioMeaning}`
+    this.audioExample.src = `${this.baseURL}/${this.wordData.audioExample}`
+
+    this.audioWord.play()
+    this.playStates.audioWord = true
+
+    this.audioWord.addEventListener('ended', () => {
+      this.playStates.audioWord = false
+      this.audioMeaning.play()
+      this.playStates.audioMeaning = true
+    })
+
+    this.audioMeaning.addEventListener('ended', () => {
+      this.playStates.audioMeaning = false
+      this.audioExample.play()
+      this.playStates.audioExample = true
+    })
+
+    this.audioExample.addEventListener('ended', () => {
+      this.playStates.audioExample = false
+    })
   }
 
 }
