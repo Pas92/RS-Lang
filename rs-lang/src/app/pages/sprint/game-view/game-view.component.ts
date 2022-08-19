fd0201bedcd5d3c0f1e2c94b6463b39ff5d703ca
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-08-19 21:45:35 +0200

    feat: add checkWord method

--- rs-lang/src/app/pages/sprint/game-view/game-view.component.ts
+++ rs-lang/src/app/pages/sprint/game-view/game-view.component.ts
@@ -2,28 +2,42 @@ import { Component, Input, OnInit } from '@angular/core';
 import { WordData } from 'src/app/models/requests.model';
 import { WordsService } from 'src/app/services/requests/words.service';
 
+interface IWord {
+  word: string,
+  wordTranslate: string,
+  wordRus: string
+}
+
 @Component({
   selector: 'app-game-view',
   templateUrl: './game-view.component.html',
   styleUrls: ['./game-view.component.scss']
 })
+
 export class GameViewComponent implements OnInit {
   scoreLevel: number = 1
   totalScore: number = 1
-  engWord:string = 'eng'
-  ruWord: string = 'rus'
+  engWord:string = ''
+  ruWord: string = ''
+  currentWord: IWord
 
   @Input()
   group: number = 0
 
-  constructor(private wordService: WordsService) { }
+  constructor(private wordService: WordsService) {
+    this.currentWord = {
+      word: '',
+      wordTranslate: '',
+      wordRus: ''
+    }
+  }
 
   ngOnInit(): void {
     this.wordService.getData(this.group, 0).subscribe((data: WordData[]) => {
       console.log(data)
-      const randomWord = this.getRandomWord(data)
-      this.engWord = randomWord.word;
-      this.ruWord = randomWord.wordRus;
+      this.currentWord = this.getRandomWord(data)
+      this.engWord = this.currentWord.word;
+      this.ruWord = this.currentWord.wordRus;
     })
   }
 
@@ -31,7 +45,7 @@ export class GameViewComponent implements OnInit {
     return Math.floor(Math.random() * num);
   }
 
-  getRandomWord(data: WordData[]): {word: string, wordTranslate: string, wordRus: string} {
+  getRandomWord(data: WordData[]): IWord {
     const length = data.length;
     const randomNum = this.getRandomNumber(length);
     return {
@@ -41,6 +55,9 @@ export class GameViewComponent implements OnInit {
     }
   }
 
+  checkWord(): boolean {
+    return this.currentWord.wordRus === this.currentWord.wordTranslate
+  }
 
 
 }
