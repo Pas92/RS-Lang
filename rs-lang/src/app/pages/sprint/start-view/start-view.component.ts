eee7c3413d250a0a32bad3c68c6c9b30c52f0cf1
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-08-20 20:00:43 +0200

    feat: add color to clicked level

--- rs-lang/src/app/pages/sprint/start-view/start-view.component.ts
+++ rs-lang/src/app/pages/sprint/start-view/start-view.component.ts
@@ -6,6 +6,15 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./start-view.component.scss']
 })
 export class StartViewComponent implements OnInit {
+  buttons: {name: string, key: number}[] = [
+    {name: 'A1', key: 0},
+    {name: 'A2', key: 1},
+    {name: 'B1', key: 2},
+    {name: 'B2', key: 3},
+    {name: 'C1', key: 4},
+    {name: 'C2', key: 5},
+  ]
+  activeIndex: number = 0
 
   @Output()
   level: number = 0
