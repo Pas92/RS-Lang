a620647700effebfc6e1cd154f8874beb879932a
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-08-21 19:07:32 +0200

    feat: add start timer

--- rs-lang/src/app/components/timer/timer.component.ts
+++ rs-lang/src/app/components/timer/timer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-timer',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
+  maxTime: number = 60
+  timerValue: number = 60
+  spinnerValue: number = 0
+  data: number = 0
 
   constructor() { }
 
   ngOnInit(): void {
+    const obs$ = interval(1000);
+
+    obs$.pipe(takeWhile(value => value < this.maxTime))
+        .subscribe(() => {
+          this.timerValue -= 1
+        this.spinnerValue += 100/this.maxTime;
+      });
   }
 
 }
