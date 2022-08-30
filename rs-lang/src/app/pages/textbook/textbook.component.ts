5f84fce925dc172df5fdafd4ae7bc95ed10632e7
Pas92 <pavelburdovitsin92@gmail.com>
2022-08-30 17:23:35 +0300

    feat: add methods for control statistic

--- rs-lang/src/app/pages/textbook/textbook.component.ts
+++ rs-lang/src/app/pages/textbook/textbook.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { WordData } from 'src/app/models/requests.model';
+import { StatisticHandlerService } from 'src/app/services/data-handlers/statistic-handler.service';
 import { WordsService } from 'src/app/services/requests/words.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { WordsService } from 'src/app/services/requests/words.service';
 })
 export class TextbookComponent implements OnInit, OnDestroy {
 
-  constructor(private wordService: WordsService) { }
+  constructor(private wordService: WordsService, private statistics: StatisticHandlerService) { }
 
   group = '0'
   page = '0'
@@ -32,6 +33,11 @@ export class TextbookComponent implements OnInit, OnDestroy {
     this.page = localStorage?.getItem('page') || '0'
 
     this.pageStatus.fill(false)
+
+    //TODO: Delete test code
+    this.statistics.appStatistic$.subscribe(data => {
+      console.log(data)
+    })
   }
 
   getNewData(): void {
--- rs-lang/src/app/services/data-handlers/statistic-handler.service.ts
+++ rs-lang/src/app/services/data-handlers/statistic-handler.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import * as moment from 'moment';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { DailyStatistic, DEFAULT_DAILY_STATISTIC, UserStatisticsObject } from 'src/app/models/requests.model';
+import { DailyStatistic, DEFAULT_DAILY_STATISTIC, UserStatisticsObject, WordData } from 'src/app/models/requests.model';
 import { AuthService } from '../requests/auth.service';
 import { StatisticService } from '../requests/statistic.service';
+import { WordsService } from '../requests/words.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatisticHandlerService {
 
-  constructor(private statisticProvider: StatisticService, private authService: AuthService) {
+  constructor(
+    private statisticProvider: StatisticService,
+    private authService: AuthService,
+    private wordService: WordsService) {
+
+    console.log('constructor')
     this.authService.isSignIn$.subscribe(data => {
       this._isSignIn = data
     })
@@ -25,6 +31,10 @@ export class StatisticHandlerService {
       }
     })
   }
+
+  private _bestGameSeries: number = -1
+  private _gameSeries: number = -1
+  private _trackingGame: string = ''
 
   private _isSignIn: boolean = false
 
@@ -61,4 +71,128 @@ export class StatisticHandlerService {
       })
     }
   }
+
+  startTrackingStatistic(game: 'sprint' | 'audioChallenge'): void {
+    this._trackingGame = game
+    this._bestGameSeries = 0
+    this._gameSeries = 0
+  }
+
+  stopTrackingStatistic(): void {
+    this._trackingGame = ''
+    this._bestGameSeries = -1
+    this._gameSeries = -1
+  }
+
+  // Use this method in game after each answer
+  updateWordDataAndStatistic(wordData: WordData, isCorrectAnswer: boolean): void {
+    if(this._isSignIn) {
+      if(isCorrectAnswer) {
+        this.setDataForCorrectAnswer(wordData)
+      } else {
+        this.setDataForWrongAnswer(wordData)
+      }
+
+      //TODO: Send word data and statistic
+      this.statisticProvider.setStatistic(this._appStatistic).subscribe()
+      this.wordService.updateUserDataForWord(wordData._id, wordData.userWord!).subscribe()
+      this._appStatisticSubj.next(this._appStatistic)
+    }
+  }
+
+  private setDataForCorrectAnswer(wordData: WordData): void {
+    if(wordData.userWord?.optional?.rating === 5) {
+      this._appStatistic.optional!.todayStatistics.learnedWordsTotal +=1
+    }
+    this._appStatistic.optional!.todayStatistics.correctAnswersTotal += 1
+    wordData.userWord!.optional!.rating = wordData.userWord?.optional?.rating === 8 ? 8 : wordData.userWord!.optional!.rating + 1
+
+    this._gameSeries += 1
+    this._bestGameSeries = Math.max(this._gameSeries, this._bestGameSeries)
+
+    if(this._trackingGame = 'sprint') {
+      this.setDataForCorrectAnswerInSprint(wordData)
+    }
+
+    if(this._trackingGame = 'audioChallenge') {
+      this.setDataForCorrectAnswerInAudioChallenge(wordData)
+    }
+  }
+
+  private setDataForCorrectAnswerInSprint(wordData: WordData): void {
+    this.checkNewWordInSprint(wordData)
+
+    this._appStatistic.optional!.todayStatistics.sprint.correctAnswers +=1
+    wordData.userWord!.optional!.sprintTotal += 1
+
+    this._appStatistic.optional!.todayStatistics.sprint.bestSeries = this._bestGameSeries
+  }
+
+  private setDataForCorrectAnswerInAudioChallenge(wordData: WordData): void {
+    this.checkNewWordInAudioChallenge(wordData)
+
+    this._appStatistic.optional!.todayStatistics.audioChallenge.correctAnswers +=1
+    wordData.userWord!.optional!.audioChallengeTotal += 1
+
+    this._appStatistic.optional!.todayStatistics.audioChallenge.bestSeries = this._bestGameSeries
+  }
+
+  private setDataForWrongAnswer(wordData: WordData): void {
+    if(wordData.userWord?.optional?.rating === 6) {
+      this._appStatistic.optional!.todayStatistics.learnedWordsTotal -=1
+    }
+    this._appStatistic.optional!.todayStatistics.wrongAnswersTotal += 1
+    wordData.userWord!.optional!.rating = wordData.userWord?.optional?.rating === 0 ? 0 : wordData.userWord!.optional!.rating - 1
+
+    this._gameSeries = 0
+
+    if(this._trackingGame = 'sprint') {
+      this.setDataForWrongAnswerInSprint(wordData)
+    }
+
+    if(this._trackingGame = 'audioChallenge') {
+      this.setDataForWrongAnswerInAudioChallenge(wordData)
+    }
+  }
+
+  private setDataForWrongAnswerInSprint(wordData: WordData): void {
+    this.checkNewWordInSprint(wordData)
+
+    this._appStatistic.optional!.todayStatistics.sprint.wrongAnswers +=1
+    wordData.userWord!.optional!.sprintTotal += 1
+    wordData.userWord!.optional!.sprintErrors += 1
+  }
+
+  private setDataForWrongAnswerInAudioChallenge(wordData: WordData): void {
+    this.checkNewWordInAudioChallenge(wordData)
+
+    this._appStatistic.optional!.todayStatistics.audioChallenge.wrongAnswers +=1
+    wordData.userWord!.optional!.audioChallengeTotal += 1
+    wordData.userWord!.optional!.audioChallengeErrors += 1
+  }
+
+  private checkNewWordInSprint(wordData: WordData): void {
+    if(!wordData.userWord?.optional?.isUsedInSprintGame) {
+      wordData.userWord!.optional!.isUsedInSprintGame = true
+      this._appStatistic.optional!.todayStatistics.newWordsTotal += 1
+      this._appStatistic.optional!.todayStatistics.sprint.newWords += 1
+    }
+  }
+
+  private checkNewWordInAudioChallenge(wordData: WordData) {
+    if(!wordData.userWord?.optional?.isUsedInAudioChallengeGame) {
+      wordData.userWord!.optional!.isUsedInAudioChallengeGame = true
+      this._appStatistic.optional!.todayStatistics.newWordsTotal += 1
+      this._appStatistic.optional!.todayStatistics.audioChallenge.newWords += 1
+    }
+  }
+
+  incrementLearnedWordsFromTextbook(): void {
+    this._appStatistic.optional!.todayStatistics.learnedWordsTotal +=1
+  }
+
+  decrementLearnedWordsFromTextbook(): void {
+    this._appStatistic.optional!.todayStatistics.learnedWordsTotal -=1
+  }
+
 }
