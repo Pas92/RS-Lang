6037278bb1e594b47bb75c1bd4dd76db9136d7cc
Kate4ka007 <shlemena@mail.ru>
2022-09-02 01:34:23 +0300

    feat: add section about and auth

--- rs-lang/src/app/pages/main/main.component.ts
+++ rs-lang/src/app/pages/main/main.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  goToAuth() {
+    this.router.navigate(['/auth'])
+  }
+
+  goToTextBook() {
+    this.router.navigate(['/textbook'])
   }
 
 }
