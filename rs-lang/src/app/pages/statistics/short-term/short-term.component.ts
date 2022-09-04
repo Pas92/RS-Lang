d98369411b4121500d740652ddc0c62893bca235
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-09-04 22:39:34 +0200

    fix: import statistics interfaces

--- rs-lang/src/app/pages/statistics/short-term/short-term.component.ts
+++ rs-lang/src/app/pages/statistics/short-term/short-term.component.ts
@@ -1,47 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { DonutInput } from 'src/app/components/donut-chart/donut-chart.component';
-
-export interface GameStatistic {
-  newWords: number
-  correctAnswers: number
-  wrongAnswers: number
-  bestSeries: number
-}
-
-export interface DailyStatistic {
-  date: string
-  audioChallenge: GameStatistic
-  sprint: GameStatistic
-  newWordsTotal: number
-  learnedWordsTotal: number
-  correctAnswersTotal: number
-  wrongAnswersTotal: number
-}
+import { DailyStatistic, GameStatistic } from 'src/app/models/requests.model';
 
 @Component({
   selector: 'app-short-term',
   templateUrl: './short-term.component.html',
   styleUrls: ['./short-term.component.scss']
 })
+
 export class ShortTermComponent implements OnInit {
   statistics: DailyStatistic
   audioChallenge: GameStatistic
   sprint: GameStatistic
-
 
   donutData: DonutInput
 
   constructor() { }
 
   ngOnInit(): void {
-    this.audioChallenge = this.statistics.audioChallenge;
-    this.sprint = this.statistics.sprint;
-    this.donutData = {
+      this.audioChallenge = this.statistics.audioChallenge;
+      this.sprint = this.statistics.sprint;
+      this.donutData = {
       correct: this.statistics.correctAnswersTotal,
       incorrect: this.statistics.wrongAnswersTotal,
-      // correct: 5,
-      // incorrect: 5,
-    };
+    }
   }
 
   getProcent(game: GameStatistic): number {
