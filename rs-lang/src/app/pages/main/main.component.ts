399edf1e808d1ca7cedbaaebca824ab713b2ab87
Pas92 <pavelburdavitsyn92@gmail.com>
2022-09-05 23:41:20 +0300

    fix: fix main page

--- rs-lang/src/app/pages/main/main.component.ts
+++ rs-lang/src/app/pages/main/main.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from 'src/app/services/requests/auth.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  _isSignIn: boolean = false
+  private destroy$: Subject<boolean> = new Subject<boolean>()
+
+  ngOnInit(): void {
+    this.authService.isSignIn$.pipe(takeUntil(this.destroy$)).subscribe(value => {
+      this._isSignIn = value
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 
 }
