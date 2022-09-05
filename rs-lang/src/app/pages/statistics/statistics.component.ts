d6cdaee8e783af8663d1ad6a771adccedb7b4892
Kate4ka007 <shlemena@mail.ru>
2022-09-05 14:16:00 +0300

    fix: add check for data

--- rs-lang/src/app/pages/statistics/statistics.component.ts
+++ rs-lang/src/app/pages/statistics/statistics.component.ts
@@ -1,38 +1,45 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { DailyStatistic, UserStatisticsObject } from 'src/app/models/requests.model';
+import {
+  DailyStatistic,
+  UserStatisticsObject,
+} from 'src/app/models/requests.model';
 import { StatisticHandlerService } from 'src/app/services/data-handlers/statistic-handler.service';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss']
+  styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit, OnDestroy {
-
   showToday = true;
 
   showAll = false;
 
-  appStatistics: Subscription
+  appStatistics: Subscription;
 
-  statisticData: UserStatisticsObject
+  statisticData: UserStatisticsObject;
 
-  todayStatistics: DailyStatistic
+  todayStatistics: DailyStatistic;
 
-  fullStatistics: DailyStatistic[]
+  fullStatistics: DailyStatistic[];
 
-  fullStatisticArray: DailyStatistic[]
+  fullStatisticArray: DailyStatistic[];
 
-  constructor(private statHandler: StatisticHandlerService) { }
+  constructor(private statHandler: StatisticHandlerService) {}
 
   ngOnInit(): void {
-    this.appStatistics =  this.statHandler._appStatistic$.subscribe(data=> {
-      this.statisticData = data;
-      this.todayStatistics = this.statisticData.optional!.todayStatistics
-      this.fullStatistics = this.statisticData.optional!.fullStatistics
-      this.fullStatisticArray = [...this.fullStatistics, this.todayStatistics]
-      })
+    this.appStatistics = this.statHandler._appStatistic$.subscribe((data) => {
+      if(data) {
+        this.statisticData = data;
+        this.todayStatistics = this.statisticData.optional!.todayStatistics;
+        this.fullStatistics = this.statisticData.optional!.fullStatistics;
+        this.fullStatisticArray = [...this.fullStatistics, this.todayStatistics];
+        console.log();
+      }
+
+
+    });
   }
 
   showShortTerm() {
@@ -47,5 +54,5 @@ export class StatisticsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.appStatistics.unsubscribe();
-    }
+  }
 }
